00852edd5606345711f8f5e89935de8f8ce24f90
kevintran01 <Kevintran04024@gmail.com>
2018-12-02 06:22:50 -0600

    arraged code magnet

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,12 +1,40 @@
 public class HelloWorld {
-    public static void main (String[] args) {
-        int x = 1;
-        System.out.println("Before the loop");
-        while (x < 4) {
-            System.out.println("In the loop");
-            System.out.println("Value of x is: " + x);
-            x = x + 1;
+    public static void main(String[] args) {
+        String[] wordListOne = {"24/7", "multi-Tier", "30,000 foot", "B-to-B", "win-win", "front-end", "smart"};
+        String[] wordListTwo = {"empowered", "sticky", "value-added", "oriented", "centric", "outside-the-box", "positioned", "networked"};
+        String[] wordListThree = {"process", "tipping-point", "solution", "core competency", "strategy", "mindshare"};
+
+        int oneLength = wordListOne.length;
+        int twoLength = wordListTwo.length;
+        int threeLength = wordListThree.length;
+
+        //Generating three random numbers
+        int rand1 = (int) (Math.random() * oneLength);
+        int rand2 = (int) (Math.random() * twoLength);
+        int rand3 = (int) (Math.random() * threeLength);
+
+        //building a phrase
+        String phrase = wordListOne[rand1] + " " + wordListTwo[rand2] +
+                " " + wordListThree[rand3];
+
+        System.out.println("What we need is a " + phrase + ".");
+
+        int x = 3;
+        if (x > 2) {
+            System.out.print("a");
         }
-        System.out.println("This is after the loop");
+        x = x - 1;
+        System.out.print("-");
+        while (x > 0) {
+            if (x == 2) {
+                System.out.print("b c");
+            }
+            x = x - 1;
+            System.out.print("-");
+            if (x == 1) {
+                System.out.print("d");
+                x = x - 1;
+            }
+        }
     }
 }
