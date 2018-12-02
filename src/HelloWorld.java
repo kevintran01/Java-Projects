539aad6d94cc22387e02a87829f486f87e9619b3
kevintran01 <Kevintran04024@gmail.com>
2018-12-02 04:10:28 -0600

    update

---
+++ src/HelloWorld.java
@@ -0,0 +1,18 @@
+public class HelloWorld
+{
+    static String randomString = "String to print";
+
+    static final double PINUM = 3.141529;
+
+    public static void main(String[] args)
+    {
+
+        int integerOne = 22;
+
+        int integerTwo = integerOne + 1;
+
+        System.out.println(integerTwo);
+        System.out.println(randomString);
+        System.out.println(PINUM);
+    }
+}
