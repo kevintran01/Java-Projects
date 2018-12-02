bdae4002d8b1124a811569a6978a953513e76781
kevintran01 <Kevintran04024@gmail.com>
2018-12-02 05:13:44 -0600

    update

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,18 +1,12 @@
-public class HelloWorld
-{
-    static String randomString = "String to print";
-
-    static final double PINUM = 3.141529;
-
-    public static void main(String[] args)
-    {
-
-        int integerOne = 22;
-
-        int integerTwo = integerOne + 1;
-
-        System.out.println(integerTwo);
-        System.out.println(randomString);
-        System.out.println(PINUM);
+public class HelloWorld {
+    public static void main (String[] args) {
+        int x = 1;
+        System.out.println("Before the loop");
+        while (x < 4) {
+            System.out.println("In the loop");
+            System.out.println("Value of x is: " + x);
+            x = x + 1;
+        }
+        System.out.println("This is after the loop");
     }
 }
